30e185be9a813efa08be4c5173633899d26580a4
kshitij583415 <01fe21bcs314@kletech.ac.in>
2024-10-28 12:50:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+### Description
+
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone https://github.com/kshitij583415/Portfolio.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
+
+### Thanks
+
+
---
+++ src/content_option.js
@@ -0,0 +1,147 @@
+const logotext = "Kshitij";
+const meta = {
+    title: "Kshitij",
+    description: "I’m Kshitij Full stack web devloper,currently studying in Hubli",
+};
+
+const introdata = {
+    title: "I’m Kshitij",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I attend all the classes and have 100% in webd",
+    },
+    description: "My name is  Kshitij i m currently persuing BE from KLE Technological Univsersity.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "My name is Kshitij i m currently persuing BE from KLE Technological Univsersity.I'm a dedicated web developer with a passion for coding. Crafting digital experiences is not just my profession; it's where my enthusiasm lies. With a strong background in web development, I bring creativity and technical expertise to every project. I thrive on the challenges of coding and find joy in building seamless, user-friendly websites. Let's turn ideas into reality through the language of the web.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer3",
+        where: "Banglore",
+        date: "2020",
+    },
+    {
+        jobtitle: "Software Developer2",
+        where: "Pune",
+        date: "2019",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Delhi",
+        date: "2018",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "CPP",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 30,
+    },
+    {
+        name: "Nodejs",
+        value: 85,
+    },
+    {
+        name: "Mongodb",
+        value: 79,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Backend Developer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "React Developer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/originals/57/59/40/575940e83a030d14c6a7472a1ea6baad.png",
+        description: "It is a hotel booking platform that allows users to search, filter, and reserve accommodations based on their preferences.",
+        link: "https://github.com/kshitij583415/AaramKaksha",
+    },
+    {
+        img: "https://i.pinimg.com/originals/53/8c/32/538c32489d4b9ea44ff34f63e41ca14c.png",
+        description: "Experience weather at your fingertips with our Weather App. Stay informed with real-time forecasts, personalized weather updates, and a user-friendly interface that brings the latest conditions right to your device.",
+        link: "#",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkX8vOpKNkbShi7TelLa-JvKFvBxYMKl-8OQ&s",
+        description: "A Hospital Resource Management System that connects users to available blood, platelets, and oxygen in different.",
+        link: "https://github.com/kshitij583415/CareJeevan",
+    },
+   
+    
+    {
+        img: "https://th.bing.com/th/id/OIP.bf_Um5shxy9Snj8HbRR0ZAHaHa?rs=1&pid=ImgDetMain",
+        description: "Boost your productivity with our Todo List app – your ultimate task management companion. Effortlessly organize your day, set priorities, and track your accomplishments, ensuring you stay focused and on top of your goals.",
+        link: "#",
+    },
+    {
+        img: "https://lenadesign.org/wp-content/uploads/2021/03/train.jpg",
+        description: "The Railway Management application is an Object-Oriented Programming (OOP) project designed to streamline and automate various aspects of railway operations.",
+        link: "https://github.com/kshitij583415/Flight-Management",
+    },
+    {
+        img: "https://www.admissionfever.com/Media/clgimg/gallery/44384_img8167798265878378.jpg",
+        description: "I enhanced a DUGC website tailored for use by college coordinators and chairman. The website features functionalities such as ISA and lab analysis, circular eligibility checks, and course withdrawal options. ",
+        link: "https://github.com/kshitij583415/DUGC_main.git",
+    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kshitij2k04@gmail.com",
+    YOUR_FONE: "9555402098",
+    description: "Feel free to reach out! Whether you have a project in mind, want to discuss collaboration opportunities, or simply have a question, I'm always open to connecting with fellow enthusiasts, clients, or anyone with a shared interest in the world of web development. Your message is just a click away, and I look forward to hearing from you soon. Let's bring ideas to life and create something amazing together ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kshitij583415",
+    linkedin: "https://www.linkedin.com/in/kshitijsingh58",
+    twitter: "https://x.com/Kshitij52_",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio, introdata, logotext, meta, services, skills, socialprofils, worktimeline
+};
+
